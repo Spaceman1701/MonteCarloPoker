48ae0a165db0881228383f743fbf7aadb076d893
Ethan <fc.spaceman@gmail.com>
2016-04-22 15:08:33 -0600

    Add player info and pot. distribution method to PotDistributor

---
+++ src/ehunter/poker/pot/Pot.java
@@ -0,0 +1,7 @@
+package ehunter.poker.pot;
+
+/**
+ * Created by 40501 on 4/22/2016.
+ */
+public class Pot {
+}
--- src/ehunter/poker/pot/PotDistributor.java
+++ src/ehunter/poker/pot/PotDistributor.java
@@ -1,8 +1,10 @@
 package ehunter.poker.pot;
 
+import ehunter.poker.player.PlayerInfo;
+
 /**
  * Created by 40501 on 4/22/2016.
  */
 public interface PotDistributor {
-    
+    public void distributePotAmoungPlayers(Pot pot, PlayerInfo[] players);
 }
