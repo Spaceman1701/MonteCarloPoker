88ca3550b388d12206dc03300a69f2a6ecf0f16c
Ethan <fc.spaceman@gmail.com>
2016-04-27 14:18:19 -0600

    Remove excess classes

--- src/ehunter/poker/pot/PotDistributor.java
+++ src/ehunter/poker/pot/PotDistributor.java
@@ -1,10 +1,10 @@
 package ehunter.poker.pot;
 
-import ehunter.poker.player.PlayerInfo;
+import ehunter.poker.player.Player;
 
 /**
  * Created by 40501 on 4/22/2016.
  */
 public interface PotDistributor {
-    public void distributePotAmoungPlayers(Pot pot, PlayerInfo[] players);
+    public void distributePotAmoungPlayers(Pot pot, Player[] players);
 }
