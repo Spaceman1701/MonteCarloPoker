73b44da126ab2563b0795f5a279c4bebf10256e7
Ethan <fc.spaceman@gmail.com>
2016-05-06 14:39:19 -0600

    Implement second DeadChipPotDistributor test

--- test/ehunter/poker/pot/potdistributor/DeadChipPotDistributorTest.java
+++ test/ehunter/poker/pot/potdistributor/DeadChipPotDistributorTest.java
@@ -89,6 +89,66 @@ public class DeadChipPotDistributorTest {
 
     @Test
     public void testDistributePotAmongPlayersMultipleWinnersNoOddChips() {
+        Card[] tableCards = new Card[] {
+                new Card(CardValue.ACE, CardSuit.CLUBS),
+                new Card(CardValue.QUEEN, CardSuit.HEARTS),
+                new Card(CardValue.TEN, CardSuit.DIAMONDS),
+                new Card(CardValue.TWO, CardSuit.CLUBS),
+                new Card(CardValue.SEVEN, CardSuit.CLUBS)
+        };
+
+        Player straightPlayer = new Player();
+        straightPlayer.setHoleCards(new HoleCards(new Card(CardValue.KING, CardSuit.DIAMONDS),
+                new Card(CardValue.JACK, CardSuit.HEARTS)));
+
+        Player striaghtPlayer2 = new Player(); //SHOULD WIN
+        striaghtPlayer2.setHoleCards(new HoleCards(new Card(CardValue.KING, CardSuit.CLUBS), new Card(CardValue.JACK,
+                CardSuit.HEARTS)));
+
+        Player highCardPlayer = new Player();
+        highCardPlayer.setHoleCards(new HoleCards(new Card(CardValue.FOUR, CardSuit.HEARTS),
+                new Card(CardValue.SIX, CardSuit.SPADES)));
+        highCardPlayer.fold();
+
+        Game game = new Game();
+        TestUtilities.assignCommunityCards(game, tableCards);
+
+        straightPlayer.game = game;
+        striaghtPlayer2.game = game;
+        highCardPlayer.game = game;
+
+        Pot pot = new Pot();
+
+        pot.bet(new Bet(straightPlayer, 100));
+        pot.bet(new Bet(striaghtPlayer2, 100));
+        pot.bet(new Bet(highCardPlayer, 10));
+
+        DeadChipPotDistributor distributor = new DeadChipPotDistributor();
+
+        Collection<Winnings> winnings =
+                distributor.distributePotAmongPlayers(pot, new Player[]{straightPlayer, striaghtPlayer2, highCardPlayer});
+
+        assert winnings.size() == 1 : "wrong size winnings set: " + winnings.size();
+
+        for (Winnings w : winnings) {
+            Player p = w.getTargetPlayer();
+            int chips = w.getChips();
+            boolean deadChips = w.isDeadChips();
+
+            assert !deadChips;
+
+            boolean foundPlayer = false;
+            if (p == straightPlayer) {
+                foundPlayer = true;
+                assert chips == 105;
+            } else if (p == striaghtPlayer2) {
+                foundPlayer = true;
+                assert chips == 105;
+            } else if (p == highCardPlayer) {
+                foundPlayer = true;
+                assert chips == 0;
+            }
+        }
 
     }
 
